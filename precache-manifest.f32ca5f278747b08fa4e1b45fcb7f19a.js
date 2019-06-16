7f924056118c2347312182efb2452edea1b5d54e
Alan Lima <alima@outlook.com>
2019-06-16 19:33:57 +1000

    Updates

---
+++ precache-manifest.f32ca5f278747b08fa4e1b45fcb7f19a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5037f097916b4a1009f3e4ba6b47166f",
+    "url": "/cti-cloud-computing/index.html"
+  },
+  {
+    "revision": "cae10aafa2a32836be42",
+    "url": "/cti-cloud-computing/static/css/main.02c0cb27.chunk.css"
+  },
+  {
+    "revision": "b01000858c9a3ef61d2e",
+    "url": "/cti-cloud-computing/static/js/2.58f4b99d.chunk.js"
+  },
+  {
+    "revision": "cae10aafa2a32836be42",
+    "url": "/cti-cloud-computing/static/js/main.7b2388f8.chunk.js"
+  },
+  {
+    "revision": "8d96e015fe63cf5b3eec",
+    "url": "/cti-cloud-computing/static/js/runtime~main.1f369ba4.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cti-cloud-computing/precache-manifest.f32ca5f278747b08fa4e1b45fcb7f19a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cti-cloud-computing/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
