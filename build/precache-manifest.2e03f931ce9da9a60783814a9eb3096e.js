02e97e066573b8853b8606489889db6d2b59ee0b
Alan Lima <alima@outlook.com>
2019-06-16 19:26:34 +1000

    added build to repository

---
+++ build/precache-manifest.2e03f931ce9da9a60783814a9eb3096e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7758879a6f61a7fb12f7501039e266b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9876c97c592f532ed17",
+    "url": "/static/css/main.02c0cb27.chunk.css"
+  },
+  {
+    "revision": "b01000858c9a3ef61d2e",
+    "url": "/static/js/2.58f4b99d.chunk.js"
+  },
+  {
+    "revision": "f9876c97c592f532ed17",
+    "url": "/static/js/main.dd66a49b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
